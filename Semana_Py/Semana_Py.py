7896d9b8f35ed4af9277044f49589a4a928550a2
Fa <falinhe62@gmail.com>
2025-09-02 15:29:58 -0500

    Subiendo ejercicio1-Sem1

---
+++ Semana_Py/Semana_Py.py
@@ -0,0 +1,10 @@
+def ejer1():#
+    nombre=input("Ingrese su nombreP: ")
+    carrera=input("Ingrese su carreraP: ")
+
+    print(f"{nombre}, \ndBienvenido a Fa de {carrera}")
+ejer1()
+    
+
+
+    
