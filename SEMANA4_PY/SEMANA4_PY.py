980c2bb78096b8fa3059b6a41f1656e7bddfabff
Fa <falinhe62@gmail.com>
2025-09-09 15:50:50 -0500

    Semana4 ejercicio 1

---
+++ SEMANA4_PY/SEMANA4_PY.py
@@ -0,0 +1,14 @@
+def ejer1():
+    edad = int(input("Ingrese su edad: "))
+    if edad < 18:
+        print("Menor de edad")
+    else:
+        if edad >= 18 and edad <= 64:
+           print("Adulto")
+        else:
+           print("Adulto mayor")
+         
+
+
+ejer1()
+
